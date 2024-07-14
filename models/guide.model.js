f1145d487c9900c11cd4c4fd076fecd2109c9596
takbir-hasan <takbirhasan274@gmail.com>
2024-07-14 17:25:06 +0600

    t

--- models/guide.model.js
+++ models/guide.model.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const guideSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
+    username: {type: String, require: true},
     name: { type: String, required: true },
     location: { type: String, required: true },
-    rating: { type: String, required: true },
+    price: { type: String, required: true },
+    rating: { type: Number, required: true },
     image: { type: String, required: true }, // Assuming image is stored as base64 string
-    availableDates: { type: String, required: true },
-    pricePerDay: { type: String, required: true }
+    available: { type: String, required: true },
+    
 });
 
 module.exports = mongoose.model('Guide', guideSchema);
